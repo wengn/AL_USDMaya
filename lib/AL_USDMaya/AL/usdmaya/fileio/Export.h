2ff25995045f5e42e5ae5f7fd8c773b64464a427
wengn <email>
2019-01-09 02:06:07 -0500

    1.Fix bug in creating file path name when importing UsdUVTexture node 2. Fix bug in connecting mesh to shading engine after importing mesh into maya 3. Implement new way for user to explicitly select shader node and export them as separate material usd file, which deprecates Export::exportAIShader and checkShapeShading

--- lib/AL_USDMaya/AL/usdmaya/fileio/Export.h
+++ lib/AL_USDMaya/AL/usdmaya/fileio/Export.h
@@ -90,8 +90,9 @@ private:
   translators::TranslatorManufacture m_translatorManufacture;
 
 
-  void checkShapeShading(MDagPath shapePath, SdfPath& usdPath); //Naiqi's change 
+  void checkShapeShading(MDagPath shapePath, SdfPath& usdPath); //Naiqi's change, this function is deprecated due to the requirement of exporting material separately
   void exportAIShader(); //Naiqi's change
+  void exportSelectedAIShader(); // Only export selected material to individual file
   std::vector<MObjectHandle> checkFileTextureNode(const MObject& shadingEngineObj); //Naiqi's change
   std::vector<MObjectHandle> m_aiSurfaceShaders; //Naiqi's change
   std::vector<SdfPath> m_shapeUsdPaths; //Naiqi's change
