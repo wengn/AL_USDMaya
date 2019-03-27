bb7c5da195b27bb8be0f1e0e10d8917a6a5523c9
wengn <email>
2019-03-27 05:55:12 -0400

    Remove build directory

--- 2018build/plugin/AL_USDMayaPlugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/naiqi/repos/AL_USDMaya/plugin/AL_USDMayaPlugin
-# Build directory: /home/naiqi/repos/AL_USDMaya/2018build/plugin/AL_USDMayaPlugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 2018build/usdmayautils/AL/usdmaya/utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/naiqi/repos/AL_USDMaya/usdmayautils/AL/usdmaya/utils
-# Build directory: /home/naiqi/repos/AL_USDMaya/2018build/usdmayautils/AL/usdmaya/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
